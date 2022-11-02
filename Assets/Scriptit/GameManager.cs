3b586ae7b660fb2bdbb77d31ee7ba2402af0057e
Nilongg <99095762+Nilongg@users.noreply.github.com>
2022-11-02 12:32:16 +0200

    Placementti script ja vähän muuta

--- Assets/Scriptit/GameManager.cs
+++ Assets/Scriptit/GameManager.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    
     public int placedPumpkins;
 
-    void Start()
+    public TextMeshProUGUI pumpkingText;
+    
+
+    void Awake()
     {
-        
+        instance = this;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pumpkingText.text = "Pumpkins placed: " + placedPumpkins;
     }
 }

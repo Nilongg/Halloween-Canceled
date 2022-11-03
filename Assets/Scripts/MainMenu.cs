ede98e414270494e037ea8bfbd4dbaa41ae23400
Joni <Joni.finskas@gmail.com>
2022-11-03 14:07:15 +0200

    Main Menu button fixed
    Main Menu button fixed in RetryScene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,6 +11,13 @@ public class MainMenu : MonoBehaviour
         // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // MainMenu nappi toimimaan (testi)
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(SceneNimi);
+        
+    }
+
 
     public void QuitGame()
     {

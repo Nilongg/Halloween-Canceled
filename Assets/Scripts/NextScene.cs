4ee0731fa9dfd644f4283abd9a285a2fdebabec3
Nilongg <niilo.vanhatupa@outlook.com>
2022-11-03 18:36:34 +0200

    jotai

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class NextScene : MonoBehaviour
 {
-    public int nextScene = 1;
+    public string scene;
     public int seconds = 2;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,7 @@ public class NextScene : MonoBehaviour
     {
         
         yield return new WaitForSeconds(seconds);
-        SceneManager.LoadScene(nextScene);
+        SceneManager.LoadScene(scene);
         
     }
 }
